97d23e07eb22c841705a90976f20dcc71105d609
YangWooSong <90258668+YangWooSong@users.noreply.github.com>
2023-10-30 09:03:57 +0900

    SceneMove

--- Boxing/Assets/Start/Script/ChangeScene.cs
+++ Boxing/Assets/Start/Script/ChangeScene.cs
@@ -7,8 +7,30 @@ public class ChangeScene : MonoBehaviour
 {
     void Update()
     {
+        
+    }
+
+    public void MoveMain()
+    {
         // 아무 키나 누르면 Main 씬으로 넘어가기
         if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Main");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.A))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    public void Restart()
+    {
+        // 아무 키나 누르면 Main 씬으로 넘어가기
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Main");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.B))
         {
             SceneManager.LoadScene("Main");
         }
